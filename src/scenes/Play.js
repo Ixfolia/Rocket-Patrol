936285cea7c29a929d9b787cf6268ae04a890e09
Brady Lin <linbrady16877@gmail.com>
2023-10-11 21:55:57 -0700

    Created collisions, added explosion sprites. Spaceships will now go boom when hit.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,9 @@ class Play extends Phaser.Scene {
         this.load.image("rocket", "./assets/rocket.png");
         this.load.image("spaceship", "./assets/spaceship.png");
         this.load.image("starfield", "./assets/starfield.png");
+
+        // load spritesheet
+        this.load.spritesheet("explosion", "./assets/explosion.png", {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
 
@@ -35,6 +38,13 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        // animation config
+        this.anims.create({
+            key: "explode",
+            frames: this.anims.generateFrameNumbers("explosion", {start: 0, end: 9, first: 0}),
+            frameRate: 30
+        });
     }
 
     update (){
@@ -49,18 +59,21 @@ class Play extends Phaser.Scene {
 
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)){
-            console.log("kaboom ship 03");
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship03);
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)){
-            console.log("kaboom ship 02");
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship02);
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)){
-            console.log("kaboom ship 01");
-        }        
+            this.p1Rocket.reset();
+            this.shipExplode(this.ship01);
+        }
 
     }
 
-    checkCollision(rocket, ship01){
+    checkCollision(rocket, ship){
         // simple AABB checking
         if (rocket.x < ship.x + ship.width && rocket.x + rocket.width > ship.x && rocket.y < ship.y + ship.height && rocket.height + rocket.y > ship.y){
             return true;
@@ -70,5 +83,17 @@ class Play extends Phaser.Scene {
         }
     }
 
+        shipExplode(ship){
+            // temporarily hide ship
+            ship.alpha = 0;
+            // create explosion sprite at ship's position
+            let boom = this.add.sprite(ship.x, ship.y, "explosion").setOrigin(0, 0);
+            boom.anims.play("explode");                 // play explode animation
+            boom.on("animationcomplete", () => {        // callback after animation completes
+                ship.reset();                           // reset ship position
+                ship.alpha = 1;                         // make ship visible again
+                boom.destroy();                         // remove explosion sprite
+            });
+        }
 
 }
